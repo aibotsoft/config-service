ef47b52327b3dbcc6dcb07dacb7d1ade9350f0ab
aibotsoft <aibotsoft@gmail.com>
2020-05-09 19:49:40 +0300

    feat: add migration

--- migrations/20200509191537_tPort.up.sql
+++ migrations/20200509191537_tPort.up.sql
@@ -4,4 +4,11 @@ create table dbo.Port
     ServiceName varchar(100)     not null,
     GrpcPort    int              not null,
     constraint PK_Port primary key (Id),
+    constraint UI_GrpcPort unique (GrpcPort),
 )
+insert into dbo.Port (ServiceName, GrpcPort)
+values ('forted-service', 50051),
+       ('surebet-service', 50052),
+       ('sbo-service', 50053),
+       ('pin-service', 50054),
+       ('config-service', 50055)
